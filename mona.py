8399dea1d91cf12d97047e26066a5265a0e1f580
JebesonJr <jebesonjunior007@gmail.com>
2025-10-26 15:45:27 -0300

    Update and rename mona3.py to mona.py

--- mona.py
+++ mona.py
@@ -1,4 +1,4 @@
-# mona3.py
+# mona.py
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
